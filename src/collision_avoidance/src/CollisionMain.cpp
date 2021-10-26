b1a42da74801c20e6e5b30c581c586bb28bc27af
Cecilia Assolito <assolito.1857897@studenti.uniroma1.it>
2021-10-26 22:07:01 +0200

    completamento aggiunta funzioni

--- src/collision_avoidance/src/CollisionMain.cpp
+++ src/collision_avoidance/src/CollisionMain.cpp
@@ -16,10 +16,13 @@ geometry_msgs::Twist vel_utente ;
 
 geometry_msgs::Twist vel_corrette ;
 
+bool coord_arrived ;
+
 ros::Publisher vel_pub ;
 
 void LetturaVel(const geometry_msgs::Twist::ConstPtr& msg) {
 	ROS_INFO("Sto ricevendo le coordinate di dove l'utente desidera far andare il robot: x:%f y:%f z:%f", msg->linear.x, msg->linear.y, msg->angular.z);
+	coord_arrived = true ;
 	vel_utente.linear.x = msg->linear.x;
 	vel_utente.linear.y = msg->linear.y;
 	vel_utente.angular.z = msg->angular.z; 
@@ -27,22 +30,34 @@ void LetturaVel(const geometry_msgs::Twist::ConstPtr& msg) {
 
 void LaserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
 	ROS_INFO("sono entrato nel LaserCallBack \n");
-	laser_geometry::LaserProjection projector_;
-	tf::TransformListener listener_;
+	if ( coord_arrived == false ) return ;
+	coord_arrived = false ;// così da evitare di processare due volte uno stesso comando di velocità
+	//inizializzazione oggetti necessari per la procedura
+	laser_geometry::LaserProjection projector;
+	tf::TransformListener listener;
 	tf::StampedTransform transform ;
+	
+	
 	/*if (!listener_.waitForTransform(scan_in->header.frame_id,"/base_link",
 	scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
 	ros::Duration(1.0))) return ;*/
-	
 	sensor_msgs::PointCloud cloud;
 	try {
-		projector_.transformLaserScanToPointCloud("/stageros",*scan_in,cloud,listener_);
+		//otteniamo punti ostacolo dal laser scanner
+		projector.transformLaserScanToPointCloud("base_laser_link",*scan_in,cloud,listener);
+		//otteniamo la trasformata per ottenere i punti ostacolo rispetto al robot
+		listener.waitForTransform("base_footprint","base_laser_link",ros::Time(0),ros::Duration(10.0));
+		listener.lookupTransform("base_footprint","base_laser_link",ros::Time(0),transform);
 	}
 	catch ( tf::TransformException& e ) {
 		//ROS_ERROR("%s", e.what());
 		std::cout << e.what();
+		ros::Duration(1.0).sleep();
 		return;
 	}
+	
+	
+	//otteniamo la trasformata sotto forma di matrice
 	Eigen::Isometry2f transform_laser = convertPose2D(transform);
 	
 	Eigen::Vector2f p;
@@ -56,18 +71,26 @@ void LaserCallBack(const sensor_msgs::LaserScan::ConstPtr& scan_in) {
 		
 		p = transform_laser*p; //in questo modo ottengo il punto trasformato
 		
-		sum_x-= p(0) / (p(0)*p(0) + p(1)*p(1));
-		sum_y-= p(1) / (p(0)*p(0) + p(1)*p(1));
+		float modulo = 1/sqrt(point.x*point.x + point.y*point.y);
+		
+		sum_x+= p(0)*modulo*modulo;//p(0) / (p(0)*p(0) + p(1)*p(1));
+		sum_y+= p(1)*modulo*modulo;//p(1) / (p(0)*p(0) + p(1)*p(1));
 	}
+	//le forze che consideriamo hanno la stessa direzione e verso opposto dei vettori robot->punto-collisione
+	//quindi prendiamo l'opposto delle forze calcolate
+	sum_x = -sum_x ;
+	sum_y = -sum_y ;
 	
 	vel_corrette.linear.x = sum_x + vel_utente.linear.x ;
+	ROS_INFO("vel_corrette di coordinata x %f \n", vel_corrette.linear.x );
 	vel_corrette.angular.z = sum_y + vel_utente.angular.z ;
+	ROS_INFO("vel_corrette di coordinata y %f \n", vel_corrette.linear.y );
 	
 	/*vel_corrette.linear.x = vel_utente.linear.x;
 	vel_corrette.linear.y = vel_utente.linear.y;
-	vel_corrette.angular.z = vel_utente.angular.z;
+	vel_corrette.angular.z = vel_utente.angular.z;*/
 	
-	ROS_INFO("Ho settato la vel corretta a : x:%f y:%f z:%f", vel_corrette.linear.x, vel_corrette.linear.y, vel_corrette.angular.z);*/
+	ROS_INFO("Ho settato la vel corretta a : x:%f y:%f z:%f", vel_corrette.linear.x, vel_corrette.linear.y, vel_corrette.angular.z);
 	
 	ROS_INFO("Sto pubblicando le vel: x:%f y:%f z:%f", vel_corrette.linear.x, vel_corrette.linear.y, vel_corrette.angular.z);
 	
